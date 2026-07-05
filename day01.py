9448b3f17005a2ae4f5e5a317625e7fc114666fe
Amit Ranjan <amitranjan1221@gmail.com>
2026-07-05 16:38:48 +0530

    Day 1 Homework

--- day01.py
+++ day01.py
@@ -1,13 +1,16 @@
-print("="*20)
-print("Welcome to Project Phoenix!")
-print("="*20)
 
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 city = input("Enter your city: ")
+language = input("Enter your favourite programming language: ")
+
+print("="*40)
+print("Welcome to Project Phoenix!")
+print("="*40)
 
 print(f"Hello {name}!")
 print(f"You are {age} years old.")
 print(f"You live in {city}.")
-print("Have a great day")
+print(f"Your favourite programming language is {language}.")
+print("Have a great Day!")
       
