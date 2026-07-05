e0d9cfd44fc5f1967c2a159ff497173f7cb7d8cc
Amit Ranjan <amitranjan1221@gmail.com>
2026-07-05 16:26:49 +0530

    Day 1: Learned variables, input and f-strings

---
+++ day01.py
@@ -0,0 +1,13 @@
+print("="*20)
+print("Welcome to Project Phoenix!")
+print("="*20)
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+
+print(f"Hello {name}!")
+print(f"You are {age} years old.")
+print(f"You live in {city}.")
+print("Have a great day")
+      
